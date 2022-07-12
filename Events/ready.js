02de2061d8dd3539b96803439ce35169ed416cfb
AltwargEvan <evanaltwarg2021@gmail.com>
2022-07-12 03:29:22 -0400

    Ready sets bots status

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,9 @@
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setActivity('Stein\'s Gate', { type: 'WATCHING' });
+        client.user.setStatus('online');
+    },
 };
