f6f2fb6397c4ec9cba0ab67788dbf2ebe1f35986
AltwargEvan <evanaltwarg2021@gmail.com>
2022-07-12 05:02:13 -0400

    haha sussy

--- Index.js
+++ Index.js
@@ -1,56 +1,151 @@
-const fs = require('node:fs');
-const path = require('node:path');
-
-// Require the necessary discord.js classes
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, VoiceChannel, Intents } = require('discord.js');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    StreamType,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+} = require('@discordjs/voice');
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+/**
+ * 	In this example, we are creating a single audio player that plays to a number of voice channels.
+ * The audio player will play a single track.
+ */
 
-// Link commands to client
-client.commands = new Collection();
+/**
+ * Create the audio player. We will use this for all of our connections.
+ */
+const player = createAudioPlayer();
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+function playSong() {
+    /**
+     * Here we are creating an audio resource using a sample song freely available online
+     * (see https://www.soundhelix.com/audio-examples)
+     *
+     * We specify an arbitrary inputType. This means that we aren't too sure what the format of
+     * the input is, and that we'd like to have this converted into a format we can use. If we
+     * were using an Ogg or WebM source, then we could change this value. However, for now we
+     * will leave this as arbitrary.
+     */
+    const resource = createAudioResource('./Resources/AmongUsInRealLife.mp3', {
+        inputType: StreamType.Arbitrary,
+    });
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    /**
+     * We will now play this to the audio player. By default, the audio player will not play until
+     * at least one voice connection is subscribed to it, so it is fine to attach our resource to the
+     * audio player this early.
+     */
+    player.play(resource);
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
+    /**
+     * Here we are using a helper function. It will resolve if the player enters the Playing
+     * state within 5 seconds, otherwise it will reject with an error.
+     */
+    return entersState(player, AudioPlayerStatus.Playing, 5e3);
 }
 
-// Create event listeners
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+async function connectToChannel(channel) {
+    /**
+     * Here, we try to establish a connection to a voice channel. If we're already connected
+     * to this voice channel, @discordjs/voice will just return the existing connection for us!
+     */
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+    });
 
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
+    /**
+     * If we're dealing with a connection that isn't yet Ready, we can set a reasonable
+     * time limit before giving up. In this example, we give the voice connection 30 seconds
+     * to enter the ready state before giving up.
+     */
+    try {
+        /**
+         * Allow ourselves 10 seconds to join the voice channel. If we do not join within then,
+         * an error is thrown.
+         */
+        await entersState(connection, VoiceConnectionStatus.Ready, 10e3);
+        /**
+         * At this point, the voice connection is ready within 10 seconds! This means we can
+         * start playing audio in the voice channel. We return the connection so it can be
+         * used by the caller.
+         */
+        return connection;
+    } catch (error) {
+        /**
+         * At this point, the voice connection has not entered the Ready state. We should make
+         * sure to destroy it, and propagate the error by throwing it, so that the calling function
+         * is aware that we failed to connect to the channel.
+         */
+        connection.destroy();
+        throw error;
     }
 }
 
-// Handle Commands
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+/**
+ * Main code
+ * =========
+ * Here we will implement the helper functions that we have defined above.
+ */
 
-    const command = client.commands.get(interaction.commandName);
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
+});
 
-    if (!command) return;
+void client.login(token);
 
+client.on('ready', async () => {
+    console.log('Discord.js client is ready!');
+
+    /**
+     * Try to get our song ready to play for when the bot joins a voice channel
+     */
     try {
-        await command.execute(interaction);
+        await playSong();
     } catch (error) {
+        /**
+         * The song isn't ready to play for some reason :(
+         */
         console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
-// Login to Discord with your client's token
-client.login(token);
+client.on('messageCreate', async (message) => {
+    if (!message.guild) return;
+    const sussyVocabulary = ['sus', 'among us']
+    if (sussyVocabulary.includes(message.content.toLowerCase())) {
+        const channel = message.member?.voice.channel;
+        message.reply('Among 😃👿 us 🇺🇸 in 🏽 real 🔎 life 👊🧬 (sus, sus) Among 💰 us 💰👨👨 in 🏽👏 real 👹 life 💓 (sus, sus) Playing ❣️ among 💰 us 🇺🇸👨 in 👉 real 📷 life 🧬 Spaceship with my ✋🏻😀🕒 crew 👬 Gonna 🙃👈👈 split 🕺🏻 up, 😿 spread ✨ out 😝🚪 \'Cause 🐗 we 👬 all 😋💩 got 👍 tasks 🙌🤗 to do 😤 Gotta 🙈 find 🔍🔍 the imposter 🚫 As they 🤼 try 😈 to sabotage Who 🤷 can 🤦‍♂️ we 👩‍👩‍👦‍👦 trust 🫂 in 😩😂 this among 🙆🏽 us 🇺🇸 entourage ? Heard 👂🏻 a sound, 🎤 turned 😍 around 💞 Looking 👀 up, 🚘 looking 👀 down ⬇️ Then 🏿 I 🤢 find 🔎 a dead 🐩 body 💃 Gotta 👉 blow 🌬 the horn 📯 emergency 🚨 Discussion 🤔 who 👿 should 💘🤔 we 👩‍👩‍👦‍👦 believe 🙏 Can\'t 👄 decide 😱😱 so now 👇 we 👥 leave 🦋🥰 Not ❌ sure 😉🔜 what 🤪🤪 I\'m 😏 about 💥 to see 👀👀 Will 🔥 it be 👏 a dead 😳 body ? 💃 Still 🥇 have 🙋🍡🎣 a task I 🙀 must 👫 complete 🚫 Who\'s ☑️ the impostеr ? Looking 👀 sus, who ❓ can 👺 we 👦💰 trust ? 🤞 Who\'s 🤷 the imposter ? 😡😤 Looking 👀 sus, who 👀 can wе 🧚 trust ? 🫂 Who\'s 🤷 the imposter ? 🚫 Looking 👀 sus, who 🤷 can 🥫 we 👨👩 trust ? 🤞 Who\'s 😇 the imposter ? 😡😤 Looking 😏💕 sus, who 😂🤷 can we 👧 trust ? 🤞 I\'m 🤪 a ghost, 👻 I\'m 😓 my 🧜🏻‍♀️ biggest 🙀 fear 😨 Got killed… ⚰️')
+        if (channel) {
+            /**
+             * The user is in a voice channel, try to connect.
+             */
+            try {
+                const connection = await connectToChannel(channel);
+                /**
+                 * We have successfully connected! Now we can subscribe our connection to
+                 * the player. This means that the player will play audio in the user's
+                 * voice channel.
+                 */
+                connection.subscribe(player);
+                await delay(5)
+                connection.destroy()
+                playSong()
+            } catch (error) {
+                /**
+                 * Unable to connect to the voice channel within 30 seconds :(
+                 */
+                console.error(error);
+            }
+        }
+    }
+});
